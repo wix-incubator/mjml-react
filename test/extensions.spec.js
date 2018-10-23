6a257719934851426143665bc6f4b2642d25d67f
Artūras Chramčenko <arturasc@wix.com>
2018-10-23 09:56:31 +0300

    fix(mjml-react): test: more explicit "it" statement

--- test/extensions.spec.js
+++ test/extensions.spec.js
@@ -51,7 +51,7 @@ describe('extensions', () => {
   });
 
   describe('tracking pixel', () => {
-    it('should render 1x1 with provided src', () => {
+    it('should render 1x1 raw image with provided src', () => {
       const markup = renderToMjml(<MjmlTrackingPixel src={'tracking-pixel'}/>);
       expect(markup).to.include('<mj-raw><img src="tracking-pixel" style="height:1px!important;width:1px!important;border-width:0!important;margin-top:0!important;margin-bottom:0!important;margin-right:0!important;margin-left:0!important;padding-top:0!important;padding-bottom:0!important;padding-right:0!important;padding-left:0!important" width="1" height="1"/></mj-raw>');
     });
