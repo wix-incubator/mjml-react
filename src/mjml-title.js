6752691a1905cbbf42bd1e38212e7b670ec10caa
Jess Telford <jess+github@jes.st>
2019-01-22 16:24:06 +1100

    A title can contain a node/nodes which render to a string
    Stops a proptype warning when doing things like:
    
    <MjmlTitle>{this.props.name} A Special Offer For You!</MjmlTitle>

--- src/mjml-title.js
+++ src/mjml-title.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
-import {string} from 'prop-types';
+import {node} from 'prop-types';
 
 import {handleMjmlProps} from './utils';
 
 export class MjmlTitle extends Component {
 
   static propTypes = {
-    children: string.isRequired
+    children: node.isRequired
   }
 
   render() {
