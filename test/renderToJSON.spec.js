5f182fabc3fa044da8c97947904ac4f7bec71b71
Mantas Miliukas <mantasm@wix.com>
2021-02-01 22:54:24 +0200

    chore(mjml-react): move all existing tests to usecases

--- test/renderToJSON.spec.js
+++ test/renderToJSON.spec.js
@@ -17,6 +17,8 @@ import {
   MjmlHead,
 } from '../src';
 
+import { MjmlComment } from '../extensions';
+
 const useCases = [
   <Mjml />,
 
@@ -48,103 +50,31 @@ const useCases = [
       </MjmlRaw>
     </MjmlBody>
   </Mjml>,
+
+  <MjmlRaw>
+    <div dangerouslySetInnerHTML={{ __html: '<div>Hello World!</div>' }}></div>
+  </MjmlRaw>,
+
+  <Mjml>
+    <MjmlBody>
+      <MjmlSection>
+        <MjmlColumn>
+          <MjmlImage width="100px" src="/assets/img/logo-small.png" />
+          <MjmlDivider borderColor="#F45E43" />
+          <MjmlText fontSize="20px" color="#F45E43" fontFamily="helvetica">
+            Hello World
+          </MjmlText>
+        </MjmlColumn>
+      </MjmlSection>
+    </MjmlBody>
+  </Mjml>,
+
+  <MjmlComment>content</MjmlComment>,
 ];
 
 useCases.forEach((tree, i) => {
   it(`should render usecase ${i}`, () => {
     const mjml = renderToMjml(tree);
     expect(mjml2json(mjml)).to.eql(renderToJSON(tree));
-  });
-});
-
-describe('render to JSON', () => {
-  it('should render nested raw content as a string', () => {
-    const result = renderToJSON(
-      <Mjml>
-        <MjmlRaw>
-          <div>content</div>
-        </MjmlRaw>
-      </Mjml>,
-    );
-    expect(result).to.eql({
-      tagName: 'mjml',
-      children: [
-        {
-          tagName: 'mj-raw',
-          content: '<div>content</div>',
-          attributes: {},
-        },
-      ],
-      attributes: {},
-    });
-  });
-
-  it('should render <Mjml/> example with children', () => {
-    expect(
-      renderToJSON(
-        <Mjml>
-          <MjmlBody>
-            <MjmlSection>
-              <MjmlColumn>
-                <MjmlImage width="100px" src="/assets/img/logo-small.png" />
-                <MjmlDivider borderColor="#F45E43" />
-                <MjmlText
-                  fontSize="20px"
-                  color="#F45E43"
-                  fontFamily="helvetica"
-                >
-                  Hello World
-                </MjmlText>
-              </MjmlColumn>
-            </MjmlSection>
-          </MjmlBody>
-        </Mjml>,
-      ),
-    ).to.eql({
-      attributes: {},
-      children: [
-        {
-          attributes: {},
-          children: [
-            {
-              attributes: {},
-              children: [
-                {
-                  attributes: {},
-                  children: [
-                    {
-                      attributes: {
-                        src: '/assets/img/logo-small.png',
-                        width: '100px',
-                      },
-                      tagName: 'mj-image',
-                    },
-                    {
-                      attributes: {
-                        'border-color': '#F45E43',
-                      },
-                      tagName: 'mj-divider',
-                    },
-                    {
-                      attributes: {
-                        color: '#F45E43',
-                        'font-family': 'helvetica',
-                        'font-size': '20px',
-                      },
-                      content: 'Hello World',
-                      tagName: 'mj-text',
-                    },
-                  ],
-                  tagName: 'mj-column',
-                },
-              ],
-              tagName: 'mj-section',
-            },
-          ],
-          tagName: 'mj-body',
-        },
-      ],
-      tagName: 'mjml',
-    });
   });
 });
