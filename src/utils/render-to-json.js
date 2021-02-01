ff0ca2c51fe4313da8f2102a2906957d204cea71
Mantas Miliukas <mantasm@wix.com>
2021-02-01 23:19:34 +0200

    chore(mjml-react): move helper methods to bottom
    to increase readability

--- src/utils/render-to-json.js
+++ src/utils/render-to-json.js
@@ -2,75 +2,7 @@ import React from 'react';
 import ReactReconciler from 'react-reconciler';
 import ReactDOMServer from 'react-dom/server';
 
-const toReactElement = (element) => {
-  if (element.children.length === 0) {
-    return React.createElement(element.type, element.props);
-  }
-  return React.createElement(
-    element.type,
-    element.props,
-    element.children.map((child) =>
-      typeof child === 'string' ? child : toReactElement(child),
-    ),
-  );
-};
-
 const matchHtmlRegExp = /["'&<>]/;
-
-function escapeHtml(string) {
-  const str = '' + string;
-  const match = matchHtmlRegExp.exec(str);
-
-  if (!match) {
-    return str;
-  }
-
-  let escape;
-  let html = '';
-  let index;
-  let lastIndex = 0;
-
-  for (index = match.index; index < str.length; index++) {
-    switch (str.charCodeAt(index)) {
-      case 34: // "
-        escape = '&quot;';
-        break;
-      case 36: // $
-        escape = '&#36;';
-        break;
-      case 38: // &
-        escape = '&amp;';
-        break;
-      case 39: // '
-        escape = '&#x27;'; // modified from escape-html; used to be '&#39'
-        break;
-      case 60: // <
-        escape = '&lt;';
-        break;
-      case 62: // >
-        escape = '&gt;';
-        break;
-      default:
-        continue;
-    }
-
-    if (lastIndex !== index) {
-      html += str.substring(lastIndex, index);
-    }
-
-    lastIndex = index + 1;
-    html += escape;
-  }
-
-  return lastIndex !== index ? html + str.substring(lastIndex, index) : html;
-}
-
-function escapeTextForBrowser(text) {
-  if (typeof text === 'boolean' || typeof text === 'number') {
-    return '' + text;
-  }
-  return escapeHtml(text.trim());
-}
 
 const reconciler = ReactReconciler({
   supportsMutation: true,
@@ -159,4 +91,72 @@ export function renderToJSON(whatToRender) {
   const container = reconciler.createContainer({}, false, false);
   reconciler.updateContainer(whatToRender, container, null, null);
   return container.containerInfo.resultObj;
+}
+
+function toReactElement(element) {
+  if (element.children.length === 0) {
+    return React.createElement(element.type, element.props);
+  }
+  return React.createElement(
+    element.type,
+    element.props,
+    element.children.map((child) =>
+      typeof child === 'string' ? child : toReactElement(child),
+    ),
+  );
+}
+
+function escapeHtml(string) {
+  const str = '' + string;
+  const match = matchHtmlRegExp.exec(str);
+
+  if (!match) {
+    return str;
+  }
+
+  let escape;
+  let html = '';
+  let index;
+  let lastIndex = 0;
+
+  for (index = match.index; index < str.length; index++) {
+    switch (str.charCodeAt(index)) {
+      case 34: // "
+        escape = '&quot;';
+        break;
+      case 36: // $
+        escape = '&#36;';
+        break;
+      case 38: // &
+        escape = '&amp;';
+        break;
+      case 39: // '
+        escape = '&#x27;'; // modified from escape-html; used to be '&#39'
+        break;
+      case 60: // <
+        escape = '&lt;';
+        break;
+      case 62: // >
+        escape = '&gt;';
+        break;
+      default:
+        continue;
+    }
+
+    if (lastIndex !== index) {
+      html += str.substring(lastIndex, index);
+    }
+
+    lastIndex = index + 1;
+    html += escape;
+  }
+
+  return lastIndex !== index ? html + str.substring(lastIndex, index) : html;
+}
+
+function escapeTextForBrowser(text) {
+  if (typeof text === 'boolean' || typeof text === 'number') {
+    return '' + text;
+  }
+  return escapeHtml(text.trim());
 }
