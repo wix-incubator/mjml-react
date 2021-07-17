90ffd915f29cb34d1809eb92d6026ff698df723a
Victor Pontis <victor@pont.is>
2021-07-17 00:41:15 -0400

    temp commit

--- build.js
+++ build.js
@@ -1,13 +1,17 @@
 const esbuild = require("esbuild");
+const { dependencies, peerDependencies } = require("./package.json");
 
 esbuild
   .build({
     entryPoints: ["src/index.js"],
     outdir: "dist",
+
     bundle: true,
     sourcemap: true,
     // minify: true,
     format: "esm",
     target: ["es6"],
+    external: Object.keys(dependencies).concat(Object.keys(peerDependencies)),
+    logLevel: 'verbose',
   })
   .catch(() => process.exit(1));
