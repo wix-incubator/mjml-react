dd1c4ba9735663d4e23bd736ee194e59e622892e
Philip Windeyer <philipwindeyer@gmail.com>
2022-02-23 15:27:02 +1100

    add mjml-attributes, mjml-selector and mjml-html-attribute

---
+++ src/mjml-html-attribute.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { node } from "prop-types";
+
+import { handleMjmlProps } from "./utils";
+
+export class MjmlHtmlAttribute extends Component {
+  static propTypes = {
+    children: node,
+  };
+
+  render() {
+    const { children, ...rest } = this.props;
+    return React.createElement(
+      "mj-html-attribute",
+      handleMjmlProps(rest),
+      children
+    );
+  }
+}
