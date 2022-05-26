5ff73f7bc513da8294e5a87c08703216019e8b45
Evan McLaughlin <70530636+emmclaughlin@users.noreply.github.com>
2022-05-26 08:24:20 -0400

    Update prettier (#2534)

--- src/mjml-react/mjml/MjmlTable.tsx
+++ src/mjml-react/mjml/MjmlTable.tsx
@@ -2,7 +2,6 @@
  * This file is generated. Don't edit it directly.
  * Modify `scripts/generate-mjml-react.ts` to make changes to these files
  */
-
 import React from "react";
 
 import { convertPropsToMjmlAttributes } from "../utils";
